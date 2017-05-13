8877c9c2b5cb8408253e2f8faafd59ad42d0acf6
Marco Roman <marcoroman77@yahoo.com>
2017-05-13 13:17:53 -0700

    added some coloring functionality

--- app/src/main/java/practice/codesprint/MainActivity.java
+++ app/src/main/java/practice/codesprint/MainActivity.java
@@ -6,13 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button C1, C2, C3, C4, C5, C6;
-    Color[] indicator = new Color[6];
-    int[] colorIndicator = new int[5];
-    String[] labelColors = {"red", "yellow", "green", "blue", "magenta", "black"};
+    //private Color[] indicator = new Color[];
+    private int[] colorIndicator = new int[5];
+    private String[] labelColors = {"red", "yellow", "green", "blue", "magenta", "black"};
+    private Random gen = new Random();
+
+    private TextView colorLabel;
+    private Color labelColorIndicator;
+    private Button[] buttons = new Button[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +33,52 @@ public class MainActivity extends AppCompatActivity {
         C4 = (Button) findViewById(R.id.button4);
         C5 = (Button) findViewById(R.id.button5);
         C6 = (Button) findViewById(R.id.button6);
+
+        colorLabel = (TextView) findViewById(R.id.colorLabel);
+
+        onStart(null);
     }
 
     protected void onStart(View view){
-        
+        int i = gen.nextInt(5);
+        colorLabel.setText(labelColors[i]);
+
+        int j = i;
+        while(j == i){
+            j = gen.nextInt(5);
+        }
+
+        switch(j) {
+            case 0:
+                colorLabel.setTextColor(Color.RED);
+                //labelColorIndicator = Color.RED;
+                break;
+            case 1:
+                colorLabel.setTextColor(Color.YELLOW);
+                //labelColorIndicator = new Color(255, 255, 0);
+                break;
+            case 2:
+                colorLabel.setTextColor(Color.GREEN);
+                //labelColorIndicator = new java.awt.Color(0, 255, 0);
+                break;
+            case 3:
+                colorLabel.setTextColor(Color.BLUE);
+                //labelColorIndicator = new java.awt.Color(0, 0, 255);
+                break;
+            case 4:
+                colorLabel.setTextColor(Color.MAGENTA);
+                //labelColorIndicator = new java.awt.Color(255, 0, 255);
+                break;
+            case 5:
+                colorLabel.setTextColor(Color.BLACK);
+                break;
+            default:
+                break;
+        }
+
+        for(int k = 0; k < 5; ++k){
+
+        }
     }
 
 
